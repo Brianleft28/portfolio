a45d8b17102939b9023b853057aa3c73ee1963fe
Brian Benegas <bbenegas@miituzaingo.gob.ar>
2025-06-22 20:11:08 -0300

    hotfix: more keywords for seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,22 +21,20 @@
 	<meta name="author" content="Brian Benegas" />
 	<meta
 		name="keywords"
-		content="Brian Benegas, portfolio, desarrollador web, svelte, react, nodejs, javascript, programador"
+		content="Brian Benegas, portfolio, desarrollador de software, programador web, ingeniero de software, desarrollador full-stack, desarrollador front-end, desarrollador back-end, freelance, Svelte, SvelteKit, React, Nest.js, Nest, Next.js, Next, Node.js, Node, Express.js, Express, JavaScript, TypeScript, HTML5, CSS3, Tailwind CSS, API, REST API, Docker, Nginx, CI/CD, Git, GitHub, desarrollo de aplicaciones web, soluciones de software, consultoría tecnológica, optimización web, rendimiento web, diseño responsivo, accesibilidad web"
 	/>
 
 	<!-- Open Graph (para Facebook, LinkedIn, etc.) -->
-	<meta property="og:title" content="Brian Benegas - Portfolio de Desarrollo" />
+	<meta property="og:title" content="Brian Benegas - Portfolio" />
 	<meta
 		property="og:description"
 		content="Explora los proyectos y la experiencia de Brian Benegas, desarrollador de software."
 	/>
 	<meta property="og:type" content="website" />
-	<!-- IMPORTANTE: Reemplaza 'tu-dominio.com' con tu dominio real -->
 	<meta property="og:url" content="https://brianleft.com" />
 	<meta property="og:image" content="https://brianleft.com/social-preview.png" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
-
 	<!-- Twitter Card (para Twitter) -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="Brian Benegas - Portfolio" />
@@ -44,7 +42,6 @@
 		name="twitter:description"
 		content="Explora los proyectos y la experiencia de Brian Benegas, desarrollador de software especializado en tecnologías web."
 	/>
-	<!-- IMPORTANTE: Reemplaza 'tu-dominio.com' con tu dominio real -->
 	<meta name="twitter:image" content="https://brianleft.com/social-preview.png" />
 </svelte:head>
 
